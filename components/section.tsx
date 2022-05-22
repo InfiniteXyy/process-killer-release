5bb197e9d0fe15f427617995e39d4df35907f749
infiniteXyy <xuyiyangwing@gmail.com>
2022-05-22 10:11:21 +0800

    fix: dark mode issues

--- components/section.tsx
+++ components/section.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export function Section(props: { title: string; children: React.ReactNode }) {
   const { title, children } = props;
   return (
-    <section className="mx-auto mt-7 flex w-[600px] max-w-full flex-col items-center rounded-lg bg-gray-50 p-12 pt-4">
-      <h3 className="text-md mb-4 font-medium text-gray-700 underline">{title}</h3>
+    <section className="mx-auto mt-7 flex w-[600px] max-w-full flex-col items-center rounded-lg bg-gray-50 p-12 pt-4 dark:bg-neutral-900">
+      <h3 className="text-md mb-4 font-medium text-neutral-700 underline dark:text-neutral-300">{title}</h3>
       <div>{children}</div>
     </section>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Index() {
           target="_blank"
           rel="noreferrer"
           href="https://github.com/InfiniteXyy/process-killer-release/releases/"
-          className="inline-block rounded-lg bg-gray-200 px-3 py-2 text-sm font-medium hover:bg-gray-300"
+          className="inline-block rounded-lg bg-gray-200 px-3 py-2 text-sm font-medium hover:bg-gray-300 dark:text-gray-900"
         >
           Download (Windows)
         </a>
@@ -42,8 +42,8 @@ export default function Index() {
       <footer className="mt-8 text-center text-xs">
         <div>@Copyright 2022 InfiniteXyy</div>
         <div className="mt-4 flex justify-center space-x-4 text-xl">
-          <a href='https://github.com/InfiniteXyy' className="i-[logos-github-icon]" />
-          <a href='https://twitter.com/InfiniteXyy' className="i-[logos-twitter] " />
+          <a href="https://github.com/InfiniteXyy" className="dark:text-white i-[carbon-logo-github]" />
+          <a href="https://twitter.com/InfiniteXyy" className="i-[logos-twitter] " />
         </div>
       </footer>
     </>
